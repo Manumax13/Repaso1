a968db3faf3a5e4fb4ab97fa9282f50270a8de7e
Manumax13 <manumax13.pa@gmail.com>
2023-03-18 01:20:44 -0500

    Repaso1

---
+++ Controllers/RepasoFormController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Repaso1.Models; //AGREGAMOS
+
+namespace Repaso1.Controllers
+{
+    public class RepasoFormController : Controller
+    {
+        private readonly ILogger<RepasoFormController> _logger;
+
+        public RepasoFormController(ILogger<RepasoFormController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult VistaRepasoForm()
+        {
+            return View("VistaRepasoForm");
+        }
+
+[HttpPost]
+        public IActionResult create(Curso objCurso)
+        {
+            string? nom=null, ape=null;
+             nom = objCurso.nombre;
+             ape = objCurso.apellido;
+            int credito =3, subtotal=0,  valorxCredito=100;
+            double total=0, igv=0;
+            
+            //GUARDAR CURSO EN UNA VARIABLE
+             //bool active;
+
+            string listaCurso=null;
+            List<curso> course = objCurso.CursoList;
+
+            //listaSexo = objPersona.SexList == null ? "1" : "0";
+            //si es nullo  1, sino es 0
+
+            for (int i = 0; i < course.Count; i++){
+                if(course[i].active == true){
+                    listaCurso += course[i].Type+ " ";
+                    subtotal += (credito * valorxCredito);
+                }
+                
+            } 
+
+            total = subtotal*1.18;
+            igv = total-subtotal;
+
+            ViewData["Message"] =  nom + " " + ape + " , you are already register on the following course(s):";
+            ViewData["Message1"] = "Course: " + listaCurso ;
+            ViewData["Message2"] = "Subtotal: " + subtotal;
+            ViewData["Message3"] = "IGV: " + igv; 
+            ViewData["Message4"] = "Total: " + total; 
+            
+            return View("VistaSubmit");
+        }
+
+
+[HttpGet]
+public ActionResult VistaRepasoForm(Curso objCurso)
+{
+   
+       var cursos = new Curso 
+    {
+        //CHECKBOX CURSO 
+       CursoList = new List<curso>
+        {
+            new curso{ID="1" , Type = "Matematica"}, 
+            new curso{ID="2" , Type = "Lenguaje"},
+             new curso{ID="3" , Type = "Historia"}
+        }   , 
+
+         
+    } ;   
+
+    return View(cursos);//ENVIAMOS LA VARIABLE VAR
+}
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
